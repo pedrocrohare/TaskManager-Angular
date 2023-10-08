589aaf04a6cbe779894c09710b6c18f36db385c7
pedrocrohare <pedro.crohare@mercadolibre.com>
2023-10-08 15:44:00 -0300

    filter by uid

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Firestore, collectionData } from '@angular/fire/firestore';
 import { collection } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 
 interface Task {
@@ -29,14 +30,19 @@ export class TasksComponent implements OnInit {
 
   }
   ngOnInit(): void {
-  const tasksCollection = collection(this.firestore, 'tasks');
-  this.tasks$ = collectionData(tasksCollection, { idField: 'id' }).pipe(
-    map(tasks => tasks.map(task => ({
-      id: task.id,
-      title: task['title'],
-      uid: task['uid'],
-      // ...other fields
-    }) as Task))
-  );
+    onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        const userID = user.uid;
+        const tasksCollection = collection(this.firestore, 'tasks');
+        this.tasks$ = collectionData(tasksCollection, { idField: 'id' }).pipe(
+          map(tasks => tasks.map(task => ({
+            id: task.id,
+            title: task['title'],
+            uid: task['uid'],
+          }) as Task)),
+          map(tasks => tasks.filter(task => task.uid === userID))
+        );
+      }
+    });
   }
 }
